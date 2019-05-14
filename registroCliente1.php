278239884279d176bf9c75d9a48960b92d2b7e19
arturogp17 <arturogp17@hotmail.com>
2019-05-14 08:56:19 -0500

    Correccion del tamaño del poster

--- registroCliente1.php
+++ registroCliente1.php
@@ -2,29 +2,31 @@
 include('conexion.php');
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-  $email           = $_POST['email'];
-  $password        = $_POST['password'];
   $apellidoPaterno = $_POST['apellidoPaterno'];
   $apellidoMaterno = $_POST['apellidoMaterno'];
   $nombres         = $_POST['nombres'];
-  $municipio       = $_POST['municipio'];
-  $colonia         = $_POST['colonia'];
+  $email           = $_POST['email'];
+  $password        = $_POST['password'];
   $calle           = $_POST['calle']; 
   $numero          = $_POST['numero'];
+  $colonia         = $_POST['colonia'];
   $cp              = $_POST['cp'];
+  $municipio       = $_POST['municipio'];
   $telefono        = $_POST['telefono'];
-  $sql = "INSERT INTO `cliente`(`idCliente`, `apellidoPaterno`, `apellidoMaterno`, `nombres`, 
-            `municipio`, `colonia`, `calle`, `numero`, `cp`, `telefono`, `email`, `password`) 
-  VALUES (null,'$apellidoPaterno','$apellidoMaterno','$nombres','$municipio','$colonia',
-'$calle','$numero','$cp' ,'$telefono' ,'$email', '$password')";
-  $resultado = $conexion->query($sql);
-  if($resultado == false){
-    die("Error al momento de insertar en la base de datos");
-  }
-  else
+  if($email != "")
   {
-    //Aquí nos dirigimos a pagina principal del usuario
-    header('Location:principal.php');            
+    $sql = "INSERT INTO `cliente`(`idCliente`, `apellidoPaterno`, `apellidoMaterno`, `nombres`, 
+    `municipio`, `colonia`, `calle`, `numero`, `cp`, `telefono`, `email`, `password`) 
+    VALUES (null,'$apellidoPaterno','$apellidoMaterno','$nombres','$municipio','$colonia',
+    '$calle','$numero','$cp' ,'$telefono' ,'$email', '$password')";
+    $resultado = $conexion->query($sql);
+    if($resultado == false){
+      die("Error al momento de insertar en la base de datos");
+    }
+    else
+    {
+          header('Location:principal.php');            
+    }
   }
 }
 ?>
@@ -48,7 +50,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
     <h1 style="font-family: arial"><b>Complete todos los campos</b></h1>
     <hr>
     <form method="POST">
-        <hr class="bg-info">
+      <hr class="bg-info">
       <div class="form-group">
         <input type="email" class="form-control" name="email" aria-describedby="emailHelp" placeholder="ejemplo@marbaran.com">
       </div>
@@ -63,12 +65,13 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
       </div>
       <hr class="bg-info">
       <h3>Dirección Personal</h3>
-         <div class="form-inline">
-        <input type="text" class="form-control col-4" name="municipio" placeholder="Municipio">
-        <input type="text" class="form-control col-4" name="colonia" placeholder="Colonia">
-        <input type="text" class="form-control col-4" name="calle" placeholder="Calle">
+      <div class="form-inline">
+      <input type="text" class="form-control col-4" name="calle" placeholder="Calle">
         <input type="text" class="form-control col-4" name="numero" placeholder="Número">
+        <input type="text" class="form-control col-4" name="colonia" placeholder="Colonia">
         <input type="text" class="form-control col-4" name="cp" placeholder="Código Postal">
+        <input type="text" class="form-control col-4" name="municipio" placeholder="Municipio">
+        
       </div>
       <hr>
       <div class="form-group">
@@ -79,7 +82,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
       <button type="submit" class="btn btn-outline-success btn-lg btn-block "><b>Registrar</b></button>
 
     </form>
- <br>
+    <br>
   </div>
 
   <!-- Optional JavaScript -->
@@ -89,4 +92,3 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
-
